e31ebdfbb4bf57c36a54f31bfbb84c35281cb552
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-09-21 23:51:39 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 20;
+let num4 = 5;
+let mod = (num3 % num4)+4;
+
+let set = [10, 5, 20, 15];
+let max = Math.max(...set);
